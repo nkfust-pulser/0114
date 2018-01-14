adbc133b48304aa62a088d3ef5efa16549733097
nkfust-pulser <32151166+nkfust-pulser@users.noreply.github.com>
2018-01-14 23:50:52 +0800

    0114

---
+++ Project/test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+</body>
+<script>
+	var a = 100;
+	console.log(a);
+	$(document).ready(function(){
+		a = 20;
+	});
+	console.log(a);
+</script>
+</html>
